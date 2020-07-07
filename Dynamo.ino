f1a17691ed85c543c465bfc701316b7cf0f0b43f
ReluctantPirate <reluctantpirategames@gmail.com>
2020-07-06 23:43:48 -0400

    New graphics completed

--- Dynamo.ino
+++ Dynamo.ino
@@ -3,87 +3,132 @@
 //	Inspired by the game "Chicanery", by Anna Anthropy.
 //  JJ
 
-Timer readyFlash;
-bool readyLEDOn = false;
+#define HEARTBEAT_INTERVAL 2000
+Timer heartbeatTimer;
 
-int playerCount = 0;
-bool playerFound = false;
+#define DEATH_INTERVAL 500
+Timer deathTimer;
 
-int brightness = 0;
+enum gameStates {READY, PLAY, DEATH};
+int gameState = READY;
 
-enum gameStates {SETUP, READY, PLAY, DEAD};
-int gameState = SETUP;
+byte currentHue = 0;
 
-Color playerColors[6] = {RED, ORANGE, YELLOW, GREEN, BLUE, MAGENTA};
-Color playerColor;
-
-void setup()
-{
-  brightness = 255;
-  gameState = 0;
+void setup() {
   randomize();
+  newColor();
 }
 
-void loop()
-{
-  switch (gameState)
-  {
-    /********* Setup State *********/
-    case SETUP:
-      playerColor = playerColors[random(5)];
-      setColor(playerColor);
-      buttonPressed();
-      gameState = 1;
-      break;
-    /********* Ready State *********/
+void loop() {
+  switch (gameState) {
     case READY:
-      if (readyFlash.isExpired())
-      {
-        readyLEDOn = !readyLEDOn;
-        if (readyLEDOn) {
-          setColor(playerColor);
-        }
-        else {
-          setColor(OFF);
-        }
-        readyFlash.set(500);   // Flash again in 500 millseconds
-      }
-      if (buttonPressed())
-      {
-        setColor(playerColor);
-        gameState = 2;
-      }
+      readyLoop();
+      readyDisplay();
       break;
-    /********* Play State *********/
     case PLAY:
-      if (millis() % 3 == 0)
-      {
-        brightness -= 5;
-        setColor(dim(playerColor, brightness));
-      }
+      playLoop();
+      playDisplay();
+      break;
+    case DEATH:
+      deathLoop();
+      deathDisplay();
+      break;
+  }
 
-      if (buttonPressed()) {
-        brightness = 255;
-      }
-      if (brightness == 0)
-      {
-        buttonDoubleClicked(); //Avoid skipping dead state.
-        setColor(OFF);
-        gameState = 3;
-      }
-      break;
-    /********* Dead State *********/
-    case DEAD:
-      if (buttonDoubleClicked())
-      {
-        //Reset Variables, return to Ready State
-        playerColor = playerColors[random(5)];
-        setColor(playerColor);
-        buttonPressed();
-        brightness = 255;
-        readyFlash.set(500);
-        gameState = 0;
-      }
-      break;
+  //dump button presses
+  buttonPressed();
+  buttonSingleClicked();
+  buttonDoubleClicked();
+}
+
+void readyLoop() {
+  if (buttonDoubleClicked()) {
+    newColor();
+  }
+
+  if (buttonSingleClicked()) {
+    heartbeatTimer.set(HEARTBEAT_INTERVAL);
+    gameState = PLAY;
+  }
+}
+
+void playLoop() {
+  if (buttonPressed()) {
+    heartbeatTimer.set(HEARTBEAT_INTERVAL);
+  }
+
+  if (heartbeatTimer.isExpired()) {
+    gameState = DEATH;
+    deathTimer.set(DEATH_INTERVAL);
+  }
+}
+
+void deathLoop() {
+  if (buttonDoubleClicked()) {
+    gameState = READY;
+  }
+}
+
+#define READY_PULSE_INTERVAL 1000
+#define READY_PULSE_FRAMES 10
+#define PULSE_BRIGHTNESS 100
+
+void readyDisplay() {
+  byte currentFrame = (millis() % READY_PULSE_INTERVAL) / (READY_PULSE_INTERVAL / READY_PULSE_FRAMES);
+  byte frameProgress = map((millis() % READY_PULSE_INTERVAL) % (READY_PULSE_INTERVAL / READY_PULSE_FRAMES), 0, (READY_PULSE_INTERVAL / READY_PULSE_FRAMES), 0, PULSE_BRIGHTNESS);
+
+  setColor(OFF);
+  //setColor(dim(WHITE, frameProgress));
+  FOREACH_FACE(f) {
+    if (currentFrame == (f - 1) ) {
+      setColorOnFace(makeColorHSB(currentHue, 110, frameProgress), f);
+    } else if (currentFrame == f) {
+      setColorOnFace(makeColorHSB(currentHue, 110, PULSE_BRIGHTNESS), f);
+    } else if (currentFrame == (f + 1)) {
+      setColorOnFace(makeColorHSB(currentHue, 110, PULSE_BRIGHTNESS - frameProgress), f);
+    }
+  }
+
+  setColorOnFace(makeColorHSB(currentHue, 255, 255), 0);
+}
+
+void newColor() {
+  currentHue = random(50) * 5;
+}
+
+void playDisplay() {
+  byte heartbeatProgress = map(heartbeatTimer.getRemaining(), 0, HEARTBEAT_INTERVAL, 0, 255);
+  //so we need to animate each "group" of faces slightly differently
+
+  //face 0 does its own thing
+  setColorOnFace(makeColorHSB(currentHue, 255 - heartbeatProgress, 255), 0);
+
+  //faces 1 and 5 fade the slowest
+  setColorOnFace(makeColorHSB(currentHue, 255 - heartbeatProgress, heartbeatProgress), 1);
+  setColorOnFace(makeColorHSB(currentHue, 255 - heartbeatProgress, heartbeatProgress), 5);
+
+  //face 3 fades the fastest
+  byte quickProgress = max((heartbeatProgress * 2) - 255, 0);
+  setColorOnFace(makeColorHSB(currentHue, 255 - heartbeatProgress, quickProgress), 3);
+
+  //faces 2 and 4 fade the next fastest
+  byte midProgress = (heartbeatProgress + quickProgress) / 2;
+  setColorOnFace(makeColorHSB(currentHue, 255 - heartbeatProgress, midProgress), 2);
+  setColorOnFace(makeColorHSB(currentHue, 255 - heartbeatProgress, midProgress), 4);
+}
+
+void deathDisplay() {
+
+  FOREACH_FACE(f) {
+    byte deathProgress = map(deathTimer.getRemaining(), 0, DEATH_INTERVAL, 0, 255);
+    byte currentBrightness = max(deathProgress, random(50));
+
+    if (f > 0) { //regular faces
+      setColorOnFace(makeColorHSB(currentHue, 255, currentBrightness), f);
+    } else {//face 0
+      byte filamentBrightness = max(currentBrightness, 100);
+      setColorOnFace(makeColorHSB(currentHue, 255, filamentBrightness), f);
+    }
+
   }
 }
